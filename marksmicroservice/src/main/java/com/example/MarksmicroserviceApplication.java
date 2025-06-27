bab2d1e34ccae183baecd226aea26208e1dad7b3
Saisagar1710 <bhoopathi.sai.sagar@ibm.com>
2025-06-27 12:18:55 +0530

    marksmicroservice

---
+++ marksmicroservice/src/main/java/com/example/MarksmicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarksmicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarksmicroserviceApplication.class, args);
+	}
+
+}
